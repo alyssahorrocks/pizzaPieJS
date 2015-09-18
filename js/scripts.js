d298f5fa63df80c4cb6a994270ac8b289768c3e4
Alyssa Horrocks <am.rocks@outlook.com>
2015-09-18 14:36:59 -0400

    Allow user to create another pizza to order.

--- js/scripts.js
+++ js/scripts.js
@@ -16,46 +16,48 @@ Pizza.prototype.cost = function() {
 
   var totalCost = this.quantity * (sizeCost + (this.meatTop * 2) + (this.vegTop * 1));
   return totalCost;
-}
+};
 
 
-  // $("#addPizza").click(function() {
-  //   $("#newPizzas").append("<div class="newPizza">" +
-  //               "<form id="newPizza">" +
-  //                 "<div class="form-group">" +
-  //                   "<label for="newSize">Select a Size:</label>" +
-  //                   "<input type="button" value="Small" id="newSizeSm">" +
-  //                   "<input type="button" value="Medium" id="newSizeMd">" +
-  //                   "<input type="button" value="Large" id="newSizeLg">" +
-  //                 "</div>" +
-  //
-  //                 "<div class="form-group">" +
-  //                   "<label class="checkbox-inline" for="newMeat">Select any Meat Toppings:</label>" +
-  //                   "<input type="checkbox" id="inlineCheckbox1" value="meat1"> Organic Pepperoni" +
-  //                   "<input type="checkbox" id="inlineCheckbox2" value="meat1"> Fennel Sausage" +
-  //                   "<input type="checkbox" id="inlineCheckbox3" value="meat1"> BBQ Chicken" +
-  //                   "<input type="checkbox" id="inlineCheckbox4" value="meat1"> Seschuan Beef" +
-  //                   "<input type="checkbox" id="inlineCheckbox5" value="meat1"> Applewood Bacon" +
-  //                 "</div>" +
-  //
-  //                 "<div class="form-group">" +
-  //                   "<label class="checkbox-inline" for="newVeg">Select any Meat Toppings:</label>" +
-  //                   "<input type="checkbox" id="inlineCheckbox1" value="veg1"> Sesame Kale" +
-  //                   "<input type="checkbox" id="inlineCheckbox2" value="veg1"> Roasted Garlic" +
-  //                   "<input type="checkbox" id="inlineCheckbox3" value="veg1"> Carmelized Onion" +
-  //                   "<input type="checkbox" id="inlineCheckbox4" value="veg1"> Toasted Pine Nuts" +
-  //                   "<input type="checkbox" id="inlineCheckbox5" value="veg1"> Fresh Green Peppers" +
-  //                   "<input type="checkbox" id="inlineCheckbox6" value="veg1"> Heirloom Tomato Slices" +
-  //                   "<input type="checkbox" id="inlineCheckbox7" value="veg1"> Rosemary Sauteed Mushrooms" +
-  //                 "</div>" +
-  //
-  //                 "<div class="form-group">" +
-  //                   "<label for="newQuantity">How many pies?</label>" +
-  //                   "<input type="text" id="newQuantity" placeholder="Ex. 2">" +
-  //                 "</div>" +
-  //                 "<br>");
-  // });
+
 $(document).ready(function() {
+  $("#addPizza").click(function() {
+    $("#pizzas").append("<div class='pizza'>" +
+                          "<form id='newPizza'>" +
+                            "<div class='radio-inline'>" +
+                              "<label><input type='radio' name='optradio' value='Small'id='size'>Small</label><br>" +
+                              "<label><input type='radio' name='optradio' value='Medium' id='size'>Medium</label><br>" +
+                              "<label><input type='radio' name='optradio' value='Large'id='size'>Large</label><br>" +
+                            "</div>" +
+
+                            "<div class='form-group'>" +
+                              "<label class='checkbox-inline' for='newMeat'>Select any Meat Toppings:</label><br>" +
+                              "<input type='checkbox' class='meat'> Organic Pepperoni" +
+                              "<input type='checkbox' class='meat'> Fennel Sausage" +
+                              "<input type='checkbox' class='meat'> BBQ Chicken" +
+                              "<input type='checkbox' class='meat'> Seschuan Beef" +
+                              "<input type='checkbox' class='meat'> Applewood Bacon" +
+                            "</div>" +
+
+                            "<div class='form-group'>" +
+                              "<label class='checkbox-inline' for='newVeg'>Select any Vegetable Toppings:</label><br>" +
+                              "<input type='checkbox' class='veg'> Sesame Kale" +
+                              "<input type='checkbox' class='veg'> Roasted Garlic" +
+                              "<input type='checkbox' class='veg'> Carmelized Onion" +
+                              "<input type='checkbox' class='veg'> Toasted Pine Nuts" +
+                              "<input type='checkbox' class='veg'> Fresh Green Peppers" +
+                              "<input type='checkbox' class='veg'> Heirloom Tomato Slices" +
+                              "<input type='checkbox' class='veg'> Rosemary Sauteed Mushrooms" +
+                            "</div>" +
+
+                            "<div class='form-group'>" +
+                              "<label for='newQuantity'>How many pies?</label>" +
+                              "<input type='text' id='newQuantity' placeholder='Ex. 2'>" +
+                            "</div>" +
+                          "</div>" +
+                        "</div>" );
+  });
+
   $("form#newPizza").submit(function(event) {
     var size = $("#size").val();
     var meatTop = $("input.meat:checked").length;
