e1c0d5348a4d6c77439bd37dca61abad3b16cfbb
Alyssa Horrocks <am.rocks@outlook.com>
2015-09-18 12:37:21 -0400

    Setup form for pizza input.

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe('Pizza', function() {
-  it('establish a pizza with a size, number of meat toppings, number of vegetable toppings', function() {
+  it('establish a pizza with a size, number of meat toppings, number of vegetable toppings and quantity', function() {
     var testPizza = new Pizza("Small", 1, 1, 2);
     expect(testPizza.size).to.equal("Small");
     expect(testPizza.meatTop).to.equal(1);
